8b561d82822b4b9041c9e5992f4c1a00490b9b4c
domagoj <domagoj.kriskovic@gmail.com>
2019-10-12 09:30:58 +0200

    serverless.yml in settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
+import * as path from 'path'
 import { FunctionHandlersProvider } from './functionHandlersProvider'
 import { TreeItem } from './TreeItem'
 
@@ -8,6 +9,8 @@ import { TreeItem } from './TreeItem'
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   const functionHandlersProvider = new FunctionHandlersProvider()
+
+  functionHandlersProvider.serverlessPath = transformPaths(getPaths())
 
   vscode.window.registerTreeDataProvider(
     'fnHandlerList',
@@ -24,18 +27,59 @@ export function activate(context: vscode.ExtensionContext) {
 
   vscode.commands.registerCommand(
     'fnHandlerList.openFunction',
-    async (treeItem: TreeItem) => {
-      
-    }
+    async (treeItem: TreeItem) => {}
   )
 
-  vscode.commands.registerCommand(
-    'fnPerStage.refreshEntry',
-    () =>
-      // customTreeProvider.refresh()
-      null
+  vscode.commands.registerCommand('fnHandlerList.addService', async () => {
+    const paths = await vscode.window.showOpenDialog({
+      canSelectFiles: true,
+      canSelectFolders: false,
+      canSelectMany: false,
+      filters: {
+        ServerlessDefinition: ['yml']
+      },
+      openLabel: 'Add service',
+      defaultUri: vscode.workspace.workspaceFolders[0].uri
+    })
+
+    if (!paths ||!paths[0]) {
+      return false
+    }
+
+    const rootPath = vscode.workspace.workspaceFolders[0].uri.path
+
+    vscode.workspace
+      .getConfiguration()
+      .update('serverlessmonitor.serverlessYmlPaths', [
+        ...getPaths(),
+        path.relative(rootPath, paths[0].path)
+      ])
+  })
+
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeConfiguration(e => {
+      if (e.affectsConfiguration('serverlessmonitor.serverlessYmlPaths')) {
+        functionHandlersProvider.serverlessPath = transformPaths(getPaths())
+        functionHandlersProvider.refresh()
+      }
+    })
   )
+
+  vscode.commands.registerCommand('fnHandlerList.refreshEntry', () => {
+    functionHandlersProvider.refresh()
+  })
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function getPaths(): string {
+  return vscode.workspace
+    .getConfiguration()
+    .get('serverlessmonitor.serverlessYmlPaths')
+}
+
+function transformPaths(savedPaths) {
+  const rootPath = vscode.workspace.workspaceFolders[0].uri.path
+  return savedPaths.map(savedPath => path.join(rootPath, savedPath))
+}
--- src/functionHandlersProvider.ts
+++ src/functionHandlersProvider.ts
@@ -1,45 +1,46 @@
 import * as vscode from 'vscode'
-import * as path from 'path'
 import * as fs from 'fs'
 import * as YAML from 'yaml'
 import { TreeItem, ServerlessYML } from './TreeItem'
 
 export class FunctionHandlersProvider
   implements vscode.TreeDataProvider<TreeItem> {
+  private _onDidChangeTreeData: vscode.EventEmitter<TreeItem | null> = new vscode.EventEmitter<TreeItem | null>()
+  readonly onDidChangeTreeData: vscode.Event<TreeItem | null> = this
+    ._onDidChangeTreeData.event
+  public serverlessPath: string[] = []
+
+  refresh() {
+    this._onDidChangeTreeData.fire()
+  }
+
   getTreeItem(element: TreeItem): vscode.TreeItem {
     return element
   }
 
   async getChildren(element?: TreeItem): Promise<TreeItem[]> {
     if (!element) {
-      const serverlessPath = vscode.workspace.workspaceFolders
-        ? path.join(
-            vscode.workspace.workspaceFolders[0].uri.path,
-            'serverless.yml'
-          )
-        : null
+      const services = this.serverlessPath
+        .filter(serverlessPath => this.pathExists(serverlessPath))
+        .map(serverlessPath => {
+          const serverlessFile = fs.readFileSync(serverlessPath, 'utf8')
+          const serverlessJSON: ServerlessYML = YAML.parse(serverlessFile)
 
-      if (serverlessPath && this.pathExists(serverlessPath)) {
-        const serverlessFile = fs.readFileSync(serverlessPath, 'utf8')
-        const serverlessJSON: ServerlessYML = YAML.parse(serverlessFile)
-
-        const serviceName = serverlessJSON.service.name
-
-        return [
-          new TreeItem(
+          return new TreeItem(
             {
-              label: serviceName,
+              label: serverlessJSON.service.name,
               serverlessJSON,
               serverlessPath: serverlessPath.split('/serverless.yml')[0],
               type: 'service'
             },
             vscode.TreeItemCollapsibleState.Expanded
           )
-        ]
-      } else {
+        })
+
+      if (services.length === 0) {
         vscode.window.showInformationMessage('Workspace has no serverless.yml')
-        return []
       }
+      return services
     } else if (element.settings.type === 'service') {
       return Object.keys(element.settings.serverlessJSON.functions).map(
         fnName => {
