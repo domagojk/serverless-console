eb3ff4910d023d99cc6dcb2a8e9c119bc0a5a5a0
domagoj <domagoj.kriskovic@gmail.com>
2019-11-11 15:44:50 +0100

    load more support

--- src/extension.ts
+++ src/extension.ts
@@ -120,6 +120,7 @@ export async function activate(context: vscode.ExtensionContext) {
                 try {
                   const logStreams = await cloudwatchlogs
                     .describeLogStreams({
+                      nextToken: message.payload.nextToken,
                       descending: true,
                       logGroupName: message.payload.logGroupName
                     })
@@ -128,6 +129,7 @@ export async function activate(context: vscode.ExtensionContext) {
                   treeItem.panel.webview.postMessage({
                     messageId: message.messageId,
                     payload: {
+                      nextToken: logStreams.nextToken,
                       logStreams: logStreams.logStreams
                     }
                   })
